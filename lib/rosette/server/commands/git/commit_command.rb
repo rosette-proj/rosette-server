34b5208c9b409c07abc5fda4fd29a0bad219f874
Cameron Dutro <camertron@gmail.com>
2014-10-10 10:35:04 -0700

    Specify correct ruby version in gemfile

--- lib/rosette/server/commands/git/commit_command.rb
+++ lib/rosette/server/commands/git/commit_command.rb
@@ -12,12 +12,21 @@ module Rosette
           commit_processor.process_each_phrase(repo_name, commit_id) do |phrase|
             datastore.store_phrase(repo_name, phrase)
           end
+
+          trigger_hooks
         end
 
         private
 
         def commit_processor
           @commit_processor ||= Rosette::Core::CommitProcessor.new(configuration)
+        end
+
+        def trigger_hooks
+          repo_config = get_repo(repo_name)
+          repo_config.hooks.fetch(:commit, []).each do |hook_proc|
+            hook_proc.call(configuration, repo_config, commit_id)
+          end
         end
       end
 
