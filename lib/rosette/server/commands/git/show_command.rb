473eee39b392e02f086dad5754688880a10dbd57
Matt Low <mlow@lumoslabs.com>
2014-10-27 16:33:34 -0700

    Add parent phrases case for when there is no parent

--- lib/rosette/server/commands/git/show_command.rb
+++ lib/rosette/server/commands/git/show_command.rb
@@ -16,9 +16,12 @@ module Rosette
           child_phrases = datastore.phrases_by_commits(repo_name, child_snapshot).to_a
           paths = child_phrases.map(&:file).uniq
 
-          parent_commit = repo.find_first_non_merge_parent(commit_id)
-          parent_snapshot = take_snapshot(repo, parent_commit.getId.name, paths)
-          parent_phrases = datastore.phrases_by_commits(repo_name, parent_snapshot).to_a
+          parent_phrases = if parent_commit = repo.find_first_non_merge_parent(commit_id)
+            parent_snapshot = take_snapshot(repo, parent_commit.getId.name, paths)
+            datastore.phrases_by_commits(repo_name, parent_snapshot).to_a
+          else
+            []
+          end
 
           compare(child_phrases, parent_phrases)
         end
