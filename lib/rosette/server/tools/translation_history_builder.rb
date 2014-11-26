188d02baa84f6ced68890cff76aa1deeaa0ba705
Cameron Dutro <camertron@gmail.com>
2014-11-26 11:20:35 -0800

    Refactor translation history builder
    The translation history builder walks over all commits looking for
    changes to source files (i.e. files that contain english phrases). It
    keeps track of source changes and continues searching through the
    commit history looking for target changes (changes to files that
    contain translations). When another source change is found, it extracts
    translations from the current target files and associates them with the
    current source phrases.

--- lib/rosette/server/tools/translation_history_builder.rb
+++ lib/rosette/server/tools/translation_history_builder.rb
@@ -1,5 +1,3 @@
-# encoding: UTF-8
-
 require 'concurrent'
 require 'progress-reporters'
 
@@ -12,7 +10,9 @@ module Rosette
 
       # Walks the commits in a repo and imports translations for all of them.
       class TranslationHistoryBuilder
-        attr_reader :config, :repo_config, :extractor
+        THREAD_POOL_SIZE = 10
+
+        attr_reader :config, :repo_config, :extractor_configs
         attr_reader :error_reporter, :progress_reporter
         attr_reader :path_matcher
 
@@ -20,22 +20,61 @@ module Rosette
           @config = options.fetch(:config)
           @repo_config = options.fetch(:repo_config)
           @path_matcher = options.fetch(:path_matcher)
-          @extractor = options.fetch(:extractor, repo_config.extractor_configs.first.extractor)
+          @extractor_configs = options.fetch(:extractors, repo_config.extractor_configs)
           @error_reporter = options.fetch(:error_reporter, Rosette::Core::NilErrorReporter.instance)
           @progress_reporter = options.fetch(:progress_reporter, ProgressReporters::NilProgressReporter.instance)
         end
 
         def execute
-          commit_count = config.datastore.unique_commit_count(repo_config.name)
-          pool = Concurrent::FixedThreadPool.new(10)
+          rev_walk = RevWalk.new(repo_config.repo.jgit_repo).tap do |rev_walk|
+            rev_walk.markStart(repo_config.repo.all_heads(rev_walk))
+            rev_walk.sort(RevSort::REVERSE)
+          end
 
-          config.datastore.each_unique_commit(repo_config.name) do |commit_id|
-            pool << Proc.new do
-              process_commit(commit_id)
+          rev_iterator = rev_walk.iterator
+
+          diff_finder = Rosette::Core::DiffFinder.new(
+            repo_config.repo.jgit_repo, rev_walk
+          )
+
+          process_commits(rev_iterator, diff_finder)
+        end
+
+        protected
+
+        def process_commits(rev_iterator, diff_finder)
+          # source changed
+          #   add to list of commits to update
+          # target changed
+          #   stop iterating and deal with it
+          # source AND target changed
+          #   both of the above, but retain the commit to use as a source commit next time around
+
+          source_commits = []
+          pool = Concurrent::FixedThreadPool.new(THREAD_POOL_SIZE)
+
+          while cur_commit = rev_iterator.next
+            diff = diff_finder.diff_with_parent(cur_commit)
+
+            if source_changed?(diff)
+              if target_changed?(diff)
+                # source AND target changed
+                source_commits << cur_commit
+                process_changes(pool, source_commits.dup, cur_commit, target_changes(diff))
+                source_commits.clear
+              else
+                # only source changed
+                source_commits << cur_commit
+              end
+            elsif target_changed?(diff)
+              # only target changed
+              process_changes(pool, source_commits.dup, cur_commit, target_changes(diff))
+              source_commits.clear
             end
           end
 
           pool.shutdown
+
           last_completed_count = 0
 
           while pool.shuttingdown?
@@ -43,7 +82,7 @@ module Rosette
 
             if current_completed_count > last_completed_count
               progress_reporter.report_progress(
-                current_completed_count, commit_count
+                current_completed_count, pool.scheduled_task_count
               )
             end
 
@@ -51,75 +90,92 @@ module Rosette
           end
 
           progress_reporter.report_progress(
-            pool.completed_task_count, commit_count
+            pool.completed_task_count, pool.scheduled_task_count
           )
 
           progress_reporter.report_complete
         end
 
-        protected
+        # source means English in most cases
+        def source_changed?(diff)
+          diff.any? do |entry|
+            extractor_configs.any? do |extractor_config|
+              extractor_config.matches?(entry.getNewPath)
+            end
+          end
+        end
+
+        def target_changed?(diff)
+          diff.any? do |entry|
+            path_matcher.matches?(entry.getNewPath)
+          end
+        end
+
+        # target means foreign language(s) in most cases
+        def target_changes(diff)
+          diff.each_with_object([]) do |entry, paths|
+            if path_matcher.matches?(entry.getNewPath)
+              paths << entry.getNewPath
+            end
+          end
+        end
+
+        def process_changes(pool, source_commits, target_commit, target_change_arr)
+          pool << Proc.new do
+            object_reader = repo_config.repo.jgit_repo.newObjectReader
+
+            tree_walk = TreeWalk.new(repo_config.repo.jgit_repo).tap do |tree_walk|
+              tree_walk.addTree(target_commit.getTree)
+              tree_walk.setFilter(PathFilterGroup.createFromStrings(target_change_arr))
+              tree_walk.setRecursive(true)
+            end
+
+            while tree_walk.next
+              bytes = object_reader.open(tree_walk.getObjectId(0)).getBytes
+              file_contents = Java::JavaLang::String.new(bytes, 'UTF-8').to_s  # @TODO: encoding??
+              path = tree_walk.getPathString
+              commit_ids = source_commits.map do |source_commit|
+                source_commit.getId.name
+              end
+
+              if locale = deduce_locale(repo_config, path)
+                commit_ids.each do |commit_id|
+                  trans_count = import_translations(path, file_contents, commit_id, locale)
+
+                  config.datastore.add_or_update_commit_log_locale(
+                    commit_id, locale, trans_count
+                  )
+                end
+              end
+            end
+          end
+        end
 
         def deduce_locale(commit_id, path)
           raise NotImplementedError
         end
 
-        def process_commit(commit_id)
-          significant_paths_for(commit_id).each_pair do |path, parent_commit_id|
-            file_contents = read_object(parent_commit_id, path)
-
-            if locale = deduce_locale(repo_config, parent_commit_id, path)
-              trans_count = import_translations(path, file_contents, commit_id, locale)
-
-              config.datastore.add_or_update_commit_log_locale(
-                commit_id, locale, trans_count
-              )
-            end
-          end
-        rescue => e
-          error_reporter.report_error(e)
-        end
-
-        def significant_paths_for(commit_id)
-          take_snapshot(commit_id).select do |path, parent_commit_id|
-            path_matcher.matches?(path)
-          end
-        end
-
-        def read_object(commit_id, path)
-          tree_walk = TreeWalk.new(repo_config.repo.jgit_repo)
-          tree_walk.addTree(repo_config.repo.get_rev_commit(commit_id).getTree)
-          tree_walk.setFilter(PathFilter.create(path))
-          tree_walk.setRecursive(true)
-          tree_walk.next
-          object_reader = repo_config.repo.jgit_repo.newObjectReader
-          bytes = object_reader.open(tree_walk.getObjectId(0)).getBytes
-          file_contents = Java::JavaLang::String.new(bytes, 'UTF-8').to_s  # @TODO: encoding??
-        end
-
-        def take_snapshot(commit_id)
-          Rosette::Core::SnapshotFactory.new
-            .set_repo(repo_config.repo)
-            .set_start_commit(repo_config.repo.get_rev_commit(commit_id))
-            .take_snapshot
-        end
-
         def import_translations(path, file_contents, commit_id, locale)
           trans_counter = 0
 
-          extractor.extract_each_from(file_contents) do |phrase|
-            attrs = {
-              commit_id: commit_id,
-              file: path,
-              locale: locale,
-              translation: phrase.key,
-              meta_key: phrase.meta_key
-            }
+          extractor_configs.each do |extractor_config|
+            extractor_config.extractor.extract_each_from(file_contents) do |phrase|
+              attrs = {
+                commit_id: commit_id,
+                file: path,
+                locale: locale,
+                translation: phrase.key,
+                meta_key: phrase.meta_key
+              }
 
-            begin
-              config.datastore.add_or_update_translation(repo_config.name, attrs)
-              trans_counter += 1
-            rescue Rosette::DataStores::Errors::PhraseNotFoundError => e
-              # error_reporter.report_error(e)
+              begin
+                config.datastore.add_or_update_translation(repo_config.name, attrs)
+                trans_counter += 1
+              rescue Rosette::DataStores::Errors::PhraseNotFoundError => e
+                # These can occur frequently since there may be translations
+                # that have no corresponding phrase. Reporting them was turning
+                # out to make the logs really noisy.
+              end
             end
           end
 
