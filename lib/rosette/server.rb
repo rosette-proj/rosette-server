4435723409e76be1babf3a307095b40f9c5064ae
Cameron Dutro <camertron@gmail.com>
2015-07-27 10:19:48 -0700

    Autoload tools

--- lib/rosette/server.rb
+++ lib/rosette/server.rb
@@ -4,5 +4,6 @@ module Rosette
   module Server
     autoload :Builder, 'rosette/server/builder'
     autoload :ApiV1,   'rosette/server/api_v1'
+    autoload :Tools,   'rosette/server/tools'
   end
 end
